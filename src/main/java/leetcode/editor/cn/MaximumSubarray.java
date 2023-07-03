231565c2b956f8e02bf6844730e229aedac50c29
Jacckx <68081752+Jacckx@users.noreply.github.com>
2023-07-04 03:21:23 +0800

    [53] 最大子序和 / [509] 斐波那契数

---
+++ src/main/java/leetcode/editor/cn/MaximumSubarray.java
@@ -0,0 +1,69 @@
+//给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。 
+//
+// 子数组 是数组中的一个连续部分。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+//输出：6
+//解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [1]
+//输出：1
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [5,4,-1,7,8]
+//输出：23
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 10⁵ 
+// -10⁴ <= nums[i] <= 10⁴ 
+// 
+//
+// 
+//
+// 进阶：如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的 分治法 求解。 
+//
+// Related Topics 数组 分治 动态规划 👍 6137 👎 0
+
+package leetcode.editor.cn;
+
+public class MaximumSubarray {
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int count = 0;
+        int sum = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            count += nums[i];
+            sum = Math.max(sum, count); // 取区间累计的最大值（相当于不断确定最大子序终止位置）
+            if (count <= 0) { // 如果区间累计为负数了，重置最大子序起始位置
+                count = 0;
+            }
+        }
+        return sum;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+    
+    public static void main(String[] args) {
+        Solution solution = new MaximumSubarray().new Solution();
+    }
+}
